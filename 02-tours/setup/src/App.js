cfaf99dcd2021ad72bb237d634b1050313bccf34
loco9939 <kls9939@naver.com>
2023-01-31 20:54:20 +0900

    feat: create Tours, Tour Components
    - fetch 사용법을 익혔다.
    - p 태그 안에 button 태그 가능하다는 것을 배웠다.
    - 데이터와 데이터 변경함수를 같은 곳에 위치시켜서 자식 컴포넌트가 데이터 변경함수를 props로 전달받아 사용하는 것을 배웠다.
    - useEffect를 라이프 사이클 주기별로 원하는 시점에 코드를 실행하는 방법을 배웠다.

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    setTours((prevTours) => prevTours.filter((prevTour) => prevTour.id !== id));
+  };
+
+  const fetchTour = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTour();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button onClick={fetchTour} className="btn">Refresh</button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section className="tours">
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      {tours.map((tour) => (
+        <Tour key={tour.id} removeTour={removeTour} {...tour} />
+      ))}
+    </section>
+  );
 };
 
 export default Tours;
